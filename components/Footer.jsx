0f36d4b0709a25c0765ddd444330012aa2383dff
Your Name <you@example.com>
2022-06-04 17:41:59 +0100

    first commit

---
+++ components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai';
+const date = new Date().getFullYear();
+
+const Footer = () => {
+  return (
+    <div className='footer-container'>
+      <p>{date} OMF Headphones All rights reserverd</p>
+      <p className='icons'>
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
+};
+
+export default Footer;

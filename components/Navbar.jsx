f34a87dd8ff64e34385335dd34fb8d059d35d128
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2022-06-05 18:48:37 +0100

    Update Navbar.jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='navbar-container'>
       <p className='logo'>
-        <Link href='/'>OMF Headphones</Link>
+        <Link href='/'>OMF Gadgets Store</Link>
       </p>
 
       <button

dc2da4e3396aa9f4ecc2837814e65326e5d921cd
Oyedele_Musa_Funso <56215827+mfoyedele@users.noreply.github.com>
2022-06-05 18:32:15 +0100

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -17,7 +17,7 @@ const HeroBanner = ({ heroBanner }) => {
         />
 
         <div>
-          <Link href={`/product/${heroBanner.product}`}>
+          <Link href='/product/smart-watch'>
             <button type='button'>{heroBanner.buttonText}</button>
           </Link>
           <div className='desc'>
